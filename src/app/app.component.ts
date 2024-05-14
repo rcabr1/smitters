ed811ab5c3240a005fcb3bd932d3daf609bc95de
Raul Cabrera <rcabr31@gmail.com>
2024-05-14 19:38:32 -0300

    feat: setting basic estructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormTeamComponent } from './form-team/form-team.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormTeamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'project-smitters-sorter-teams';
 }
